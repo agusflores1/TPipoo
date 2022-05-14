7110edd203ed27a0b98064d73bdc5fdc15d908a2
Agustina Flores <90777261+agusflores1@users.noreply.github.com>
2022-05-14 15:54:15 -0300

    Update testViaje.php

--- testViaje.php
+++ testViaje.php
@@ -1,9 +1,15 @@
 <?php
 include "viaje.php";
+//TP3
+include "ViajeTerrestre.php";
+include "ViajeAerolinea.php";
+//
 include "pasajero.php";
 include "responsableV.php";
 
 $arrayPasajeros=[];
+$empresa="Empresa Transporte";
+$importe=100;
 
 echo "\n BIENVENIDO A VIAJE FELIZ \n
 Ingrese los datos del viaje:  \n";
@@ -16,23 +22,73 @@ echo "Ingrese cantidad maxima de pasajeros: \n ";
 $cantMax=trim(fgets(STDIN));
 echo "DATOS EMPLEADO RESPONSABLE DE VIAJE:  \n";
 $objResponsableV=ingresarDatosEmpleado();
-$objViaje=new viaje($codigo,$destino,$cantMax,$objResponsableV,$arrayPasajeros);
+
+$objViaje=new viaje($codigo,$destino,$cantMax,$objResponsableV,$arrayPasajeros,$importe,0);
 
 do{
 echo "MENU. \n
-1. Agregar Pasajero \n
-2. Quitar Pasajero. \n
-3. Modificar Pasajero. \n 
-4. Modificar el código del viaje.\n
-5. Modificar el destino del viaje.\n
-6. Modificar la cantidad de asientos del viaje. \n
-7. Modificar datos del Responsable de viaje. \n
-8. Ver viaje. \n
-9. Salir. \n"; ;
+1. Vender Viaje
+2. Agregar Pasajero \n
+3. Quitar Pasajero. \n
+4. Modificar Pasajero. \n 
+5. Modificar el código del viaje.\n
+6. Modificar el destino del viaje.\n
+7. Modificar la cantidad de asientos del viaje. \n
+8. Modificar datos del Responsable de viaje. \n
+9. Ver viaje. \n
+10. Salir. \n"; ;
 $rta=trim(fgets(STDIN));
  switch($rta)
 { 
 case 1 :
+       if($objViaje->disponibilidadLugar()==true)
+       { 
+        echo "QUE TIPO DE VIAJE PREFIERE? 1 (AEREO) o 2  (TERRESTRE): \n";
+        $rta=trim(fgets(STDIN)); 
+
+        if($rta==1)
+        { $nombreAerolinea=$empresa;
+         
+        echo "viaje de (Ida y Vuelta) o (Ida): ";
+        $tipoViaje=trim(fgets(STDIN));
+        echo "INGRESE LOS DATOS DE VUELO: ";
+        echo "Numero de Vuelo: ";
+        $nroVuelo=trim(fgets(STDIN));
+        echo "Categoria (Primera Clase) o (Turista): ";
+        $categoria=trim(fgets(STDIN));
+        echo "Cuantas escalas desea hacer? ";
+        $escalas=trim(fgets(STDIN));
+
+        $objViajeAereo=new ViajeAerolinea($codigo,$destino,$cantMax,$objResponsableV,$arrayPasajeros,$importe,$tipoViaje,$nroVuelo,$categoria,$nombreAerolinea,$escalas);
+        $pasajeroNuevo=recogerDatos();
+        echo $objViajeAereo;
+        $total=$objViajeAereo->venderPasaje($pasajeroNuevo);
+          //Agrego pasajero para poder visualizarlo en la opcion 9
+        $objViaje->agregarPasajero($pasajeroNuevo);
+        echo "\nPasaje vendido con exito! Total: ".$total."\n";
+        }
+
+        if($rta==2)
+        {
+        echo "ELIJA TIPO DE ASIENTO (Cama) o (Semicama): \n";
+        $tipoAsiento=trim(fgets(STDIN));
+        echo "viaje de (Ida y Vuelta) o (Ida): \n";
+        $tipoViaje=trim(fgets(STDIN));
+        $objViajeTerrestre=new ViajeTerrestre($codigo,$destino,$cantMax,$importe,$tipoViaje,$objResponsableV,$arrayPasajeros,$tipoAsiento);
+        $pasajeroNuevo2=recogerDatos();
+        echo $objViajeTerrestre;
+        $total=$objViajeTerrestre->venderPasaje($pasajeroNuevo2);
+        //Agrego pasajero para poder visualizarlo en la opcion 9
+        $objViaje->agregarPasajero($pasajeroNuevo);
+        echo "Pasaje vendido con exito! Total: ".$total."\n";
+        }
+
+}   
+
+     else{echo "\nNo hay Disponibilidad de pasajes! \n";}
+        
+        break;
+case 2 :
         if($objViaje->disponibilidadLugar()==true)
         {$pasajero = recogerDatos();
         if($objViaje->agregarPasajero($pasajero)==false){
@@ -50,7 +106,7 @@ case 1 :
                  "-------------------------------------------\n";}            
         break;
   
-case 2 :
+case 3 :
         echo "Ingrese los datos del pasajero que desea eliminar: \n";
         $pasajero = recogerDatos();
         //luego de recoger los datos, eliminamos y verificamos que sus datos hayan sido borrados
@@ -64,7 +120,7 @@ case 2 :
               "-------------------------------------------\n";}
         break;
 
-case 3 :
+case 4 :
       echo "Ingrese los datos del pasajero que quiere modificar: \n";
       $pasajero=recogerDatos();
       echo "Ingrese los datos del pasajero nuevo: \n";
@@ -78,7 +134,7 @@ case 3 :
                "-------------------------------------------\n";}
      break;
 
-case 4 :
+case 5 :
         echo "El codigo de el viaje es: {$objViaje->getCodigoViaje()}. \n";
         echo "Ingrese el nuevo código: \n";
         $codigoNuevo = trim(fgets(STDIN));
@@ -86,14 +142,14 @@ case 4 :
         echo "el nuevo codigo es ".$objViaje->getCodigoViaje()."\n";
         break;
 
-case 5:
+case 6:
         echo "El viaje posee el destino: {$objViaje->getDestino()}. \n";
         echo "Ingrese el nuevo destino: \n";
         $destinoNuevo = trim(fgets(STDIN));
         $objViaje->setDestino($destinoNuevo);
         echo "el nuevo destino es: ".$objViaje->getDestino()."\n";
         break;
-case 6 :
+case 7 :
         echo "Cantidad maxima de pasajeros: {$objViaje->getCantMax()}. \n";
         echo "Ingrese la nueva cantidad maxima: \n";
         $cantMaxN= trim(fgets(STDIN));
@@ -102,18 +158,21 @@ case 6 :
         break;
 
 
-case 7 :
+case 8 :
        $nuevoEmpleado=ingresarDatosEmpleado();
        $objViaje->setResponsable($nuevoEmpleado);
          break;
 
-case 8 :
-        echo $objViaje;
+case 9 : 
+       
+       echo $objViaje;
+    
+       // 
         break;
 
-    
-}
-} while($rta<>9);
+
+
+}} while($rta<>10);
 
 
 
@@ -145,8 +204,8 @@ case 8 :
     $numeroLicencia= trim(fgets(STDIN));
     $objResponsableV=new responsableV($numeroEmpleado,$numeroLicencia,$nombreEmpleado,$apellidoEmpleado);
    return $objResponsableV;
-    
-
-
 }
+
+
+
 ?>
